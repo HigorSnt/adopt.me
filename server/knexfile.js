d2b6e35ef3434d7e97b17107fc026b4972912e8d
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2020-11-26 14:00:11 -0300

    correções no backend

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
     },
